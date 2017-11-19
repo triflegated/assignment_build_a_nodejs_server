0bc8d7dab8da48ec361461664843ae03c81aacd9
triflegated <adatepe.justin@gmail.com>
2017-11-18 23:00:36 -0500

    Displaying Request and Response Data using stringify and replace

--- app.js
+++ app.js
@@ -6,24 +6,62 @@ const fs = require('fs');
 
 const port = 3000;
 const host = 'localhost';
-
+const path = './public/index.html';
 
 const server = http.createServer((req,res)=> {
-	try {
+	/*try {
 		var data = fs.readFileSync('./index.html', 'utf8');
 	}
-	catch (err) {
+	catch (err) {	
 		console.error(err);
 	}
-
 	if (data)
 		{
-			res.writeHead(200, {'Content-type': 'text/html'});
+			res.writeHead(200, {'Content-Type': 'text/html'});
 			res.end(data);
 	
 		} 
+	}
+*/
+	var data = fs.readFile(path, 'utf8', (err,data) =>{
+		if (err) {
+			throw err;
+		}
+		else {
+			res.writeHead(200, {'Content-Type':'text/html'});
+			
+		
 
+		let stripReq={};
+		let stripRes={};
+
+		stripReq.url=req.url;
+		stripReq.method=req.method;
+		stripReq.httpVersion=req.httpVersion;
+		stripReq.headers=req.headers;
+
+		stripRes.statusMessage=res.statusMessage;
+		stripRes.statusCode=res.statusCode;
+		stripRes._header=res._header;
+ 
+
+		//console.log(req);
+		//console.log('\n\n\n--------------------------------------');
+		//console.log(res);
+
+		var strReq = JSON.stringify(stripReq, null, 2);
+		var strRes = JSON.stringify(stripRes, null, 2);
+
+
+
+		data=data.replace('{{ res }}', strRes);
+		data=data.replace('{{ req }}', strReq);
+		res.end(data);
+
+		}
 	});
+});
+
 
 server.listen(port,host,()=>
 {
